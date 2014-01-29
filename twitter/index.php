20604c52ab5263b48c935939a137c3f20879ab91
Jonathan Querubina <jonathan@phocus.com.br>
2014-01-29 21:16:21 -0200

    API da API do Twitter

--- twitter/index.php
+++ twitter/index.php
@@ -9,7 +9,35 @@ require_once('twitteroauth.php');
 
 $tweet = new TwitterOAuth($consumerKey, $consumerSecret, $oAuthToken, $oAuthSecret);
 
-$followers = json_decode($tweet->get('followers/ids', array('screen_name' => 'johnchronus')),true);
+$user = '';
+
+if(isset($_GET['user'])){
+    $user = $_GET['user'];
+}else{
+    $user = 'johnchronus';
+}
+
+$followers = json_decode($tweet->get('followers/ids', array('screen_name' => $user)),true);
+
+$ids = $followers['ids'];
+$total = count($ids);
+
+$users = array();
+
+for ($i=0; $i<10; $i++) {
+    $user = rand(0, $total-1);
+    $users[] = $ids[$user];
+}
+
+$profile_images = array();
+
+foreach ($users as $id) {
+    $profile_image = json_decode($tweet->get('users/lookup', array('user_id' => $id)));
+    $profile_images[] = array('id'=>$id, 'url'=>$profile_image[0]->profile_image_url);
+}
+
+$result = json_encode($profile_images);
+
 //$result = $tweet->get('users/lookup', array('user_id' => '42886353'));
 
 echo '<pre>';
